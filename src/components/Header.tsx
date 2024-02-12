35c941b6338455d40c4c6a47705c749cdd5c5aa5
Rajdeep Singh <radewala.2@gmail.com>
2024-02-12 17:36:33 +0530

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,36 @@
-import Link from "next/link";
-import React from "react";
-import { Logo } from "./logo";
-export const Header = () => {
+export function Header() {
+  let items = ["Home", "Company", "Marketplace", "Features", "Team", "Contact"];
   return (
-    <header className="text-gray-600 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <Link
-          href={"/"}
-          className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+    <header className=" bg-black text-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-white dark:text-black">
+      <div className="flex flex-wrap justify-between items-center">
+        <div className="flex items-center">
+          <span className="self-center text-xl font-semibold whitespace-nowrap">
+            {" "}
+            Picture Element{" "}
+          </span>
+        </div>
+
+        <div
+          className="hidden justify-between items-center w-full lg:flex lg:w-auto "
+          id="mobile-menu-2"
         >
-          <Logo />
-        </Link>
-        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <Link href={"/"} className="mr-5 hover:text-gray-900">Home</Link>
-          <Link href={"/"} className="mr-5 hover:text-gray-900">About</Link>
-        </nav>
+          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            {items.map((item) => {
+              return (
+                <li key={item.toLowerCase()}>
+                  <a
+                    href="#"
+                    className="block py-2 pr-4 pl-3 rounded  lg:p-0"
+                    aria-current="page"
+                  >
+                    {item}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </header>
   );
-};
+}
