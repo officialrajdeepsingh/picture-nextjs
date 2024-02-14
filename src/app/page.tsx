f9e97b98005d3f99aa95b7993123b2f4dd74c569
Rajdeep Singh <radewala.2@gmail.com>
2024-02-14 20:04:55 +0530

    rewrite it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
-import { Picture } from "@/components/Picture";
+import { Logo } from "@/components/Logo";
 import { ShowPrefersColorTheme } from "@/components/Prefers-color-theme";
+import { ResponsivePicture } from "@/components/ResponsivePicture";
 
 export default function Home() {
 
   return (
-    <div className="h-full">
+    <div className="h-full 2xl:text-4xl">
       
       <ShowPrefersColorTheme />
 
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
-        <Picture />
+        <Logo />
       </div>
+      
+      <h1> Responsive Picture</h1>
+      <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
+        <ResponsivePicture />
+      </div>
+
+
     </div>
   );
 }
