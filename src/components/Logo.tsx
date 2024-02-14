5a5b4f60b7e5b21a8ffef8b6f3f8f144dc080d2c
Rajdeep Singh <radewala.2@gmail.com>
2024-02-14 20:05:25 +0530

    rename the component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { getImageProps } from 'next/image'
 
-export function Picture() {
+export function Logo() {
   const common = { alt: 'Logo', width: 800, height: 400, className: "mx-auto" }
   const { props: { srcSet: dark } } = getImageProps({ ...common, src: '/logo/dark.png' })
   const { props: { srcSet: light, ...rest } } = getImageProps({ ...common, src: '/logo/light.png' })
