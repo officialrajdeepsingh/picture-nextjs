73afe2c944cf4f4ae52fed36c73764690ffaca5b
Rajdeep Singh <radewala.2@gmail.com>
2024-02-14 20:05:41 +0530

    ResponsivePicture example

---
+++ src/components/ResponsivePicture.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { getImageProps } from 'next/image'
+import road from "../media/road_1280.jpg";
+import tree from "../media/tree_724.jpg";
+
+export function ResponsivePicture() {
+  const common = { alt: 'Logo', width: 1224, height: 624, className: "mx-auto" }
+  const { props: { srcSet: dark } } = getImageProps({ ...common, src: tree })
+  const { props: { srcSet: light, ...rest } } = getImageProps({ ...common, src: road })
+ 
+  return (
+  <picture>
+    <source media="(min-width: 1280px)" srcSet={dark} />
+    <source media="(min-width: 1536px)" srcSet={light} />
+    <img {...rest} />
+  </picture>
+)
+}
