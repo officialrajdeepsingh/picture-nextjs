46b7a26e51d91a8d78ede6fa946da31664558118
Rajdeep Singh <radewala.2@gmail.com>
2024-02-15 14:22:21 +0530

    adding comment

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { getImageProps } from 'next/image'
+import { getImageProps } from "next/image";
 
 export function Logo() {
-  const common = { alt: 'Logo', width: 800, height: 400, className: "mx-auto" }
-  const { props: { srcSet: dark } } = getImageProps({ ...common, src: '/logo/dark.png' })
-  const { props: { srcSet: light, ...rest } } = getImageProps({ ...common, src: '/logo/light.png' })
- 
+
+    // Common option
+  const common = { alt: "Logo", width: 800, height: 400, className: "mx-auto" };
+
+  // Pass common as an argument with src in getImageProps and destructure the output.
+  const { props: { srcSet: dark }} = getImageProps({ ...common, src: "/logo/dark.png" });
+  const {props: { srcSet: light, ...rest }} = getImageProps({ ...common, src: "/logo/light.png" });
+
   return (
-  <picture>
-    <source media="(prefers-color-scheme: dark)" srcSet={dark} />
-    <source media="(prefers-color-scheme: light)" srcSet={light} />
-    <img {...rest} />
-  </picture>
-)
+    <picture>
+      <source media="(prefers-color-scheme: dark)" srcSet={dark} />
+      <source media="(prefers-color-scheme: light)" srcSet={light} />
+      <img {...rest} />
+    </picture>
+  );
 }
--- src/components/ResponsivePicture.tsx
+++ src/components/ResponsivePicture.tsx
@@ -4,7 +4,11 @@ import road from "../media/road_1280.jpg";
 import tree from "../media/tree_724.jpg";
 
 export function ResponsivePicture() {
+
+  // Common option
   const common = { alt: 'Logo', width: 1224, height: 624, className: "mx-auto" }
+
+  // Pass common as an argument with src in getImageProps and destructure the output.
   const { props: { srcSet: dark } } = getImageProps({ ...common, src: tree })
   const { props: { srcSet: light, ...rest } } = getImageProps({ ...common, src: road })
  
